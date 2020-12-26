8a59532ddea6a574d0d7a679f0fe35d0440d96c2
Antonio Sucre <ajs2104@gmail.com>
2020-12-26 09:39:15 -0300

    Creating Entities

---
+++ Store.Domain/Entities/Entity.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Store.Domain.Entities
+{
+    public abstract class Entity
+    {
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; private set; }
+    }
+}
